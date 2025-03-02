21e407af2c9cf34c856c639e0943061be6ee3134
trungdo3224 <trungdo3224@gmail.com>
2025-03-02 13:05:30 +0700

    chore: get gemini api key from env

--- src/ai_crew/speech_to_text/speech_to_text_output.py
+++ src/ai_crew/speech_to_text/speech_to_text_output.py
@@ -1,13 +1,15 @@
 import json
 import os
 from google import genai
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Function to call the Gemini API
 def call_gemini(prompt, api_key):
   print(api_key)
   try:
-    client = genai.Client(api_key='AIzaSyADgS1H2sDDa7C6g5E0mXHeUtW_P4ctNHw')
+    client = genai.Client(api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash", contents=prompt
     )
@@ -17,7 +19,7 @@ def call_gemini(prompt, api_key):
     return None
 
 # Load the Gemini API key from the environment
-api_key = os.getenv("GEMINI_API_KEY") or 'AIzaSyADgS1H2sDDa7C6g5E0mXHeUtW_P4ctNHw0'
+api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("GEMINI_API_KEY not found in environment variables")
 
